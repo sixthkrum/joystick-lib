a99fc6435e7841d1d7d75ee8ef10bb7fc2060f3a
sixthkrum <mailbox.vikramalagh@gmail.com>
2020-03-08 14:06:16 +0530

    now the map has all three attributes of the button press, right now the library doesn't do much. Header file cleaned no longer a testing ground

--- joystick.cpp
+++ joystick.cpp
@@ -1,49 +1,13 @@
-#include <stdio.h>
+#include "joystick.h"
 
-#include <iostream>
-
-#include <sys/types.h>
-
-#include <sys/stat.h>
-
-#include <fcntl.h>
-
-#include <unistd.h>
-
-#include <stdlib.h>
-
-#include <string.h>
-
-#include <stdint.h>
-
-#include <iterator>
-
-#include <map>
-
-#define JS_EVENT_BUTTON 0x01
-
-#define JS_EVENT_AXIS 0x02
-
-#define JS_EVENT_INIT 0x80
-
-struct js_event {
-  uint32_t time;
-  short value;
-  uint8_t type;
-  uint8_t number;
-};
-
-int main()
-{
-  int fd = open ( "/dev/input/js0" , /*for blocking: O_RDONLY , non blocking: O_NONBLOCK*/ O_RDONLY );
-
-  if (fd > 0) {
+std::array < int , 3 > read_button_press( int fd , bool output_enable , bool report_button_release) {
+  if (fd > 0 && output_enable) {
     std::cout << "joystick opened" << '\n';
   }
 
-  else {
-    std::cout << "error" << '\n';
-    return 0;
+  else if( fd <= 0 && output_enable) {
+    std::cout << "no joystick available" << '\n';
+    return { -1 , 0 , 0 };
   }
 
   fd_set rfds;
@@ -53,42 +17,146 @@ int main()
   tv.tv_sec = 0;
   tv.tv_usec = 100;
 
+  FD_ZERO ( &rfds );
+  FD_SET ( fd , &rfds );
+
+  retval = select( fd + 1 , &rfds , NULL , NULL , &tv );
+
   js_event e;
 
-  while (1) {
+  if ( FD_ISSET ( fd , &rfds) ) {
+    read ( fd , &e , sizeof ( e ) );
 
-    FD_ZERO ( &rfds );
-    FD_SET ( fd , &rfds );
+  std::array < int , 3 > button;
 
-    retval = select( fd + 1 , &rfds , NULL , NULL , &tv );
-
-    if ( FD_ISSET ( fd , &rfds) ) {
-      read ( fd , &e , sizeof ( e ) );
-
-      switch ( e.type ) {
-        case JS_EVENT_AXIS: {
+    switch ( e.type ) {
+      case JS_EVENT_AXIS: {
+        if( output_enable ) {
           std::cout << "AXIS" << '\t';
           std::cout << +e.number << '\t';
           std::cout << +e.value << std::endl;
-          break;
         }
 
-        case JS_EVENT_BUTTON: {
+        if( report_button_release | e.value ) {
+          button [0] = int ( e.type );
+          button [1] = int ( e.number );
+          button [2] = int ( e.value );
+          return button;
+        }
+
+        break;
+      }
+
+      case JS_EVENT_BUTTON: {
+        if( output_enable ) {
           std::cout << "BUTTON" << '\t';
           std::cout << +e.number << '\t';
           std::cout << +e.value << std::endl;
+        }
+
+        if( report_button_release | e.value ) {
+          button [0] = int ( e.type );
+          button [1] = int ( e.number );
+          button [2] = int ( e.value );
+          return button;
+        }
+
+        break;
+      }
+
+      default: {
+        break;
+      }
+    }
+  }
+
+  return { -2 , 0 , 0 };
+}
+
+int set_key_binding( std::map < std::array < int , 3 > , int >& keybindings) {
+  initscr();
+  nodelay( stdscr , FALSE );
+  curs_set( FALSE );
+  keypad( stdscr , TRUE );
+  noecho();
+
+  int fd = open ( "/dev/input/js0" , /*for blocking: O_RDONLY , non blocking: O_NONBLOCK*/ O_RDONLY );
+
+  if (fd > 0) {
+    printw( "joystick opened" );
+    refresh();
+  }
+
+  else {
+    printw( "no joystick available" );
+    refresh();
+    endwin();
+    return 0;
+  }
+
+  while ( TRUE ) {
+    clear();
+    mvprintw( 0 , 0 , "enter key to bind buttons to: " );
+    refresh();
+
+    int key;
+    key = getch();
+    printw( "%d" , key );
+
+    mvprintw( 1 , 0 , "press buttons that will map to the key press ESC to stop: " );
+    refresh();
+
+    nodelay( stdscr , TRUE );
+
+    std::array < int , 3 > button;
+
+    while( getch() != 27 ) {
+      button = read_button_press( fd );
+
+      switch ( button [0] ) {
+        case -2: {
           break;
+        }
+
+        case -1: {
+          clear();
+          mvprintw( 2 , 0 , "the joystick has been disconnected, saving partial map and exiting");
+          refresh();
+          endwin();
+          // need to save map here
+          return -1;
         }
 
         default: {
+          printw( "%d\t" , button [1] );
+          keybindings.insert( std::pair < std::array < int , 3 > , int > ( button , key ) );
           break;
         }
-
       }
+    }
 
+    clear();
+    mvprintw( 0 , 0 , "bind another key? press y to continue");
+    refresh();
+
+    nodelay( stdscr , FALSE );
+
+    int choice;
+
+    choice = getch();
+
+    if ( choice == 'y' || choice == 'Y') {
+      continue;
+    }
+
+    else {
+      break;
     }
 
   }
 
-  return 1;
+clear();
+endwin();
+
+return 1;
 }
